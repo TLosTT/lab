78e1ebf0866b2c17c1ef5707b16dda6be51874cd
TLosTT <93385875+TLosTT@users.noreply.github.com>
2021-10-29 17:14:14 +0300

    Add files via upload
    Это вторая лабораторная с двумя заданиями соответственно.

---
+++ lab_2_1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+class Test {
+protected:
+	int W;
+	void Z(){
+		cout << "Eto zakritaya funciya Test" << endl;
+	}
+public:
+	Test() {
+		W = 1;
+	}
+	~Test() {};
+
+	friend void fun(Test _ob);
+};
+
+void fun(Test _ob) {
+	cout << _ob.W << endl;
+	_ob.Z();
+}
+
+int main() {
+	Test ob;
+	fun(ob);
+
+    return 0;
+}
---
+++ lab_2_2.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <time.h>
+#include <vector>
+#include <iomanip>
+#include <string>
+using namespace std;
+
+
+class Abonent{
+public:
+	int id;
+protected:
+	string sName;
+	string pNumber;
+public:
+	Abonent(int _id, string _sName, string _pNumber) {
+	    cout << "\nConstructor" << endl;
+		id = _id;
+		sName = _sName;
+		pNumber = _pNumber;
+	}
+	~Abonent() {
+	cout << "\nDestrucktor" << endl;
+	};
+
+
+	friend class Notebook;
+};
+
+class Notebook {
+
+public:
+
+	void change(Abonent& ob, string _newPNumber) {
+		ob.pNumber = _newPNumber;
+	}
+	void show(Abonent& ob) {
+		cout << ob.id << '|';
+		cout << ob.sName << '|';
+		cout << ob.pNumber << endl;
+		cout << '\n';
+	}
+};
+
+int main() {
+	vector<Abonent> abonentArr;
+	Notebook notebook;
+	abonentArr.push_back(Abonent(1, "Maxim", "893253564312"));
+	abonentArr.push_back(Abonent(2, "Anatoliy", "892754885944"));
+	abonentArr.push_back(Abonent(3, "Spencer", "892657978654"));
+	abonentArr.push_back(Abonent(4, "Axiom", "8927657463523"));
+	abonentArr.push_back(Abonent(5, "Verginiy", "891884857456"));
+	for (int i = 0; i < abonentArr.size(); i++) {
+		notebook.show(abonentArr[i]);
+	}
+	int nom;
+	string nom2;
+	cout<<"Vvedite id abonenta nujdaushegosya v izmenenii:";
+	cin>>nom;
+	cout<<"Na kakoi nomer?";
+	cin>>nom2;
+	for (int i = 0; i < abonentArr.size(); i++) {
+		if (abonentArr[i].id == nom) {
+			notebook.change(abonentArr[i], nom2);
+			break;
+		}
+	}
+	cout << "===========================================" << endl;
+	for (int i = 0; i < abonentArr.size(); i++) {
+		notebook.show(abonentArr[i]);
+	}
+	return 0;
+}
